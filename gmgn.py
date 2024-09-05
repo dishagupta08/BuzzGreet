421d0989d1dc801c6004bb7d1eb899108db059cc
dishagupta08 <disha.gupta1@learner.manipal.edu>
2024-09-05 18:39:19 +0530

    Create gmgn.py
    Add project

---
+++ gmgn.py
@@ -0,0 +1,14 @@
+
+# https://docs.python.org/3/library/time.html#time.strftime
+
+import datetime
+timestamp = datetime.datetime.now()
+# for i in timestamp.hour:
+#     print(i)
+if (timestamp.hour < 12):
+    print('Good morning')
+elif(timestamp.hour>=12 and timestamp.hour<18):
+    print(f'{timestamp.hour}:{timestamp.minute}:{timestamp.second}')
+    print('Good afternoon')
+else:
+    print('Good evening')
